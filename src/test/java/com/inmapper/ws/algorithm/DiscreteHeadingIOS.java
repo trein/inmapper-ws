f67e216e54595faadecd15d81741fb80603f8666
Navjot Singh <anavjot@yahoo.com>
2013-11-23 11:31:07 -0500

    Changes for Discrete Heading Angles

---
+++ src/test/java/com/inmapper/ws/algorithm/DiscreteHeadingIOS.java
@@ -0,0 +1,52 @@
+package com.inmapper.ws.algorithm;
+
+import static org.junit.Assert.assertSame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.inmapper.ws.exception.InvalidMobilePositionException;
+import com.inmapper.ws.model.to.MobilePointTo;
+import com.inmapper.ws.model.to.MobileSessionTo;
+import com.inmapper.ws.service.SessionAuditor;
+
+public class DiscreteHeadingIOS {
+
+    SessionAuditor sessionAuditor = new SessionAuditor();
+    StepDetector stepDetector = new StepDetector();
+    List<MobilePointTo> mobilePointsDiscrete = new ArrayList<MobilePointTo>();
+    List<MobilePointTo> mobilePoints;
+
+    @Test
+    public void test5Step1() throws InvalidMobilePositionException {
+        MobileSessionTo mobileSessionTo = sessionAuditor
+                .loadSession("ios-mapping/1384643993164-5d5e7dfb-7e32-4bdb-8251-e0bbb93e9cd9");
+        mobilePoints = mobileSessionTo.getPositions();
+        Double calibrationPhi = mobilePoints.get(0).getHeading();
+        for (MobilePointTo mobilePointTo : mobilePoints) {
+            if (mobilePointTo.getHeading() - calibrationPhi >= 45 && mobilePointTo.getHeading() - calibrationPhi < 135) {
+                mobilePointsDiscrete.add(new MobilePointTo(mobilePointTo.getX(), mobilePointTo.getY(), mobilePointTo
+                        .getZ(), Double.valueOf(90)));
+            } else if (mobilePointTo.getHeading() - calibrationPhi >= 135
+                    && mobilePointTo.getHeading() - calibrationPhi < 225) {
+                mobilePointsDiscrete.add(new MobilePointTo(mobilePointTo.getX(), mobilePointTo.getY(), mobilePointTo
+                        .getZ(), Double.valueOf(180)));
+            } else if (mobilePointTo.getHeading() - calibrationPhi >= 225
+                    || mobilePointTo.getHeading() - calibrationPhi < 315) {
+                mobilePointsDiscrete.add(new MobilePointTo(mobilePointTo.getX(), mobilePointTo.getY(), mobilePointTo
+                        .getZ(), Double.valueOf(270)));
+            } else {
+                mobilePointsDiscrete.add(new MobilePointTo(mobilePointTo.getX(), mobilePointTo.getY(), mobilePointTo
+                        .getZ(), Double.valueOf(0)));
+            }
+        }
+        System.out.println(mobilePointsDiscrete.size());
+        for (MobilePointTo mobilePointTo : mobilePointsDiscrete) {
+            System.out.println(mobilePointTo.getHeading());
+
+        }
+        assertSame("Test", -1, -1);
+    }
+}
